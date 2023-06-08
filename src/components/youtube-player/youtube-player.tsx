7054d634958c743ddda3e0fa3c027118bf2a0589
Kevin Le Guillou <contact@kevinleguillou.com>
2023-06-08 19:51:17 +0200

    Added Youtube recommendations div clip workaround. Added censored state with a debug button.

--- src/components/youtube-player/youtube-player.tsx
+++ src/components/youtube-player/youtube-player.tsx
@@ -1,15 +1,43 @@
 import { createEffect, createSignal } from 'solid-js'
 import YTPlayer from 'youtube-player'
+import styles from './youtube-player.module.css'
+import { YouTubePlayer } from 'youtube-player/dist/types'
 
 const YoutubePlayer = () => {
-  const [player, setPlayer] = createSignal()
+  const [player, setPlayer] = createSignal<YouTubePlayer>()
+  const [censored, setCensored] = createSignal(false)
+  const playerWrapperClasses = () => ({
+    [`${styles.playerWrapper}`]: true,
+    [`${styles.censored}`]: censored(),
+  })
   
   createEffect(() => {
-    setPlayer(YTPlayer('player'))
+    setPlayer(YTPlayer('player', {
+      playerVars: {
+        controls: 0
+      }
+    }))
+    player()?.loadVideoById('zMUhIgoj_W4')
+    player()?.mute()
+    setTimeout(() => {
+      player()?.pauseVideo()
+      player()?.seekTo(0, false)
+    }, 1000)
   })
 
+  const onCensor = () => {
+    setCensored(value => !value)
+  }
+
   return (
-    <div id='player'></div>
+    <div class={styles.container}>
+      <div classList={playerWrapperClasses()}>
+        <div id='player' class={styles.player} />
+      </div>
+      <button class={styles.censor} onClick={onCensor}>
+        {censored() ? 'Censored' : 'Censor'}
+      </button>
+    </div>
   )
 }
 
